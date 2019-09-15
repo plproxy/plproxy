662dd2200b6828906c17f9c793bd3077f12433ad
Marko Kreen <marko.kreen@veriff.net>
2019-09-15 20:29:13 +0300

    Sync tests
    Fixes: #35

--- test/sql/plproxy_init.sql
+++ test/sql/plproxy_init.sql
@@ -5,8 +5,7 @@ set client_min_messages = 'warning';
 
 \i sql/plproxy.sql
 
-set client_min_messages = 'fatal';
-create language plpgsql;
+create or replace language plpgsql;
 set client_min_messages = 'warning';
 
 -- create cluster info functions
@@ -66,17 +65,12 @@ drop database if exists test_enc_proxy;
 drop database if exists test_enc_part;
 
 \c test_part
-set client_min_messages = 'fatal';
-create language plpgsql;
+create or replace language plpgsql;
 \c test_part0
-set client_min_messages = 'fatal';
-create language plpgsql;
+create or replace language plpgsql;
 \c test_part1
-set client_min_messages = 'fatal';
-create language plpgsql;
+create or replace language plpgsql;
 \c test_part2
-set client_min_messages = 'fatal';
-create language plpgsql;
+create or replace language plpgsql;
 \c test_part3
-set client_min_messages = 'fatal';
-create language plpgsql;
+create or replace language plpgsql;
